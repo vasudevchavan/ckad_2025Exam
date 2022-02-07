a614f5624c54a65121730578ca13e45a317ec76a
vasudevdchavan <vasudevdchavan@gmail.com>
2022-02-07 16:27:01 +0530

    updated docker file

--- docker/server.py
+++ docker/server.py
@@ -4,16 +4,40 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        title = "Hello GKE!"
+        title = "Hello Blue GKE!"
         bgcolor = "blue"
-        self.render("template.html",title=title,bgclor=bgcolor)
+        self.render("template.html",title=title,bgcolor=bgcolor)
         print(self.request)
 
+""" class GreenHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = "Hello Green GKE!"
+        bgcolor = "green"
+        self.render("template.html",title=title,bgcolor=bgcolor)
+        print(self.request) """
 
+""" class RedHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = "Hello Red GKE!"
+        bgcolor = "red"
+        self.render("template.html",title=title,bgcolor=bgcolor)
+        print(self.request) """
+
+
+""" class NationHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("india.html") """
+
+
+ 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+      """   (r"/green", GreenHandler),
+        (r"/red", RedHandler),
+        (r"/nation", NationHandler) """
     ])
+
 
 
 if __name__ == "__main__":
